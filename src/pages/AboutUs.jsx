891ee78c2efea963a618eb30826443b203289dee
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-04-17 00:10:27 +0545

    ft:modification AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,33 @@
-import {Navbar} from '../components/Navbar'
+import { Navbar } from "../components/Navbar";
 
-export const AboutUs = ()=>{
-    return (
-        <>
-            <Navbar/>
-            Hello AboutUs!!
-        </>
-    )
-}
+export const AboutUs = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="about-section">
+        <div className="about-content">
+          <h1>Driving Innovations And Empowering Business</h1>
+          <p>
+            Metalogic envisions a future where technology serves as a catalyst
+            for limitless human potential, fostering innovation and empowering
+            organizations to thrive in a transformative digital era.
+          </p>
+        </div>
+      </section>
+      <div className="about-factors">
+        <div className="a_factors">
+          <div className="f_number">32+</div>
+          <div className="f_title">Project Completed</div>
+        </div>
+        <div className="a_factors">
+          <div className="f_number">50+</div>
+          <div className="f_title">Satisfied Clients</div>
+        </div>
+        <div className="a_factors">
+          <div className="f_number">18+</div>
+          <div className="f_title">Experts</div>
+        </div>
+      </div>
+    </>
+  );
+};
